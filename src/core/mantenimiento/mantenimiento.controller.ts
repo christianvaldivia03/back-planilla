861c17216573d2864cc2f727ba2ef4b2f6c54923
chriskira03 <christian.valdivia@unmsm.edu.pe>
2024-01-16 20:18:54 -0500

    Refactor MantenimientoController to include searchList endpoint

--- src/core/mantenimiento/mantenimiento.controller.ts
+++ src/core/mantenimiento/mantenimiento.controller.ts
@@ -1,34 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { MantenimientoService } from './mantenimiento.service';
-import { CreateMantenimientoDto } from './dto/create-mantenimiento.dto';
-import { UpdateMantenimientoDto } from './dto/update-mantenimiento.dto';
+import { searchListaDto } from './dto';
 
-@Controller('mantenimiento')
+@Controller('core/mantenimiento')
 export class MantenimientoController {
   constructor(private readonly mantenimientoService: MantenimientoService) {}
 
-  @Post()
-  create(@Body() createMantenimientoDto: CreateMantenimientoDto) {
-    return this.mantenimientoService.create(createMantenimientoDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.mantenimientoService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.mantenimientoService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMantenimientoDto: UpdateMantenimientoDto) {
-    return this.mantenimientoService.update(+id, updateMantenimientoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.mantenimientoService.remove(+id);
+  @Post('search-list')
+  searchList(@Body() searchListDto: searchListaDto) {
+    return this.mantenimientoService.searchList(searchListDto);
   }
 }
